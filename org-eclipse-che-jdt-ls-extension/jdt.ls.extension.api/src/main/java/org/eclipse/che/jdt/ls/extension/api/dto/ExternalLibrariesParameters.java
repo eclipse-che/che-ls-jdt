1390939eb64ad7690dcaf36448d2645acaa332d1
Valeriy Svydenko <vsvydenk@redhat.com>
2017-11-28 12:43:45 +0200

    code cleanup
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- org-eclipse-che-jdt-ls-extension/jdt.ls.extension.api/src/main/java/org/eclipse/che/jdt/ls/extension/api/dto/ExternalLibrariesParameters.java
+++ org-eclipse-che-jdt-ls-extension/jdt.ls.extension.api/src/main/java/org/eclipse/che/jdt/ls/extension/api/dto/ExternalLibrariesParameters.java
@@ -8,7 +8,7 @@
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
  */
-package org.eclipse.che.jdt.ls.extension.api.dto.externallibrary;
+package org.eclipse.che.jdt.ls.extension.api.dto;
 
 /**
  * Parameters for ExternalLibrary commands.
--- org-eclipse-che-jdt-ls-extension/jdt.ls.extension.api/src/main/java/org/eclipse/che/jdt/ls/extension/api/dto/JarEntry.java
+++ org-eclipse-che-jdt-ls-extension/jdt.ls.extension.api/src/main/java/org/eclipse/che/jdt/ls/extension/api/dto/JarEntry.java
@@ -8,7 +8,7 @@
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
  */
-package org.eclipse.che.jdt.ls.extension.api.dto.externallibrary;
+package org.eclipse.che.jdt.ls.extension.api.dto;
 
 /**
  * This class represents an entry of jar of external library.
