47adcc3042cf7b823da53e8096ea46c2263ab0d2
Mykola Morhun <mmorhun@redhat.com>
2017-12-04 10:52:26 +0200

    CHE-6907: Add effective pom command (#6)

---
+++ org-eclipse-che-jdt-ls-extension/jdt.ls.extension.core/src/main/java/org/eclipse/che/jdt/ls/extension/core/internal/pom/EffectivePomHandler.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2012-2017 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.jdt.ls.extension.core.internal.pom;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.List;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
+import org.apache.maven.project.MavenProject;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.jdt.ls.core.internal.JDTUtils;
+import org.eclipse.jdt.ls.core.internal.JavaLanguageServerPlugin;
+import org.eclipse.m2e.core.MavenPlugin;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.core.project.IMavenProjectRegistry;
+
+/** @author Mykola Morhun */
+public class EffectivePomHandler {
+
+  /**
+   * Generates effective pom for project.
+   *
+   * @param arguments single argument with project uri
+   * @param progressMonitor progress monitor
+   * @return effective pom for given maven project
+   */
+  public static String getEffectivePom(List<Object> arguments, IProgressMonitor progressMonitor) {
+    final String projectUri = (String) arguments.get(0);
+    final String projectPomUri = projectUri + "/pom.xml";
+
+    IFile pomFile = JDTUtils.findFile(projectPomUri);
+    if (pomFile == null) {
+      throw new IllegalArgumentException("Wrong path to project pom.xml: " + projectPomUri);
+    }
+
+    ensureNotCancelled(progressMonitor);
+
+    IMavenProjectRegistry mavenProjectRegistry = MavenPlugin.getMavenProjectRegistry();
+    IMavenProjectFacade mavenProjectFacade =
+        mavenProjectRegistry.create(pomFile, true, progressMonitor);
+    MavenProject mavenProject;
+    try {
+      mavenProject = mavenProjectFacade.getMavenProject(progressMonitor);
+    } catch (CoreException e) {
+      JavaLanguageServerPlugin.logException(
+          "Failed to retrieve maven project for path: '" + projectUri + '\'', e);
+      throw new RuntimeException(e);
+    }
+    Model mavenProjectModel = mavenProject.getModel();
+
+    ensureNotCancelled(progressMonitor);
+
+    StringWriter stringWriter = new StringWriter();
+    MavenXpp3Writer mavenXpp3Writer = new MavenXpp3Writer();
+    try {
+      mavenXpp3Writer.write(stringWriter, mavenProjectModel);
+    } catch (IOException e) {
+      JavaLanguageServerPlugin.logException(
+          "Failed to get effective pom for project: '" + projectUri + '\'', e);
+      throw new RuntimeException(e);
+    }
+
+    return stringWriter.toString();
+  }
+
+  private static void ensureNotCancelled(IProgressMonitor progressMonitor) {
+    if (progressMonitor.isCanceled()) {
+      throw new OperationCanceledException();
+    }
+  }
+}
