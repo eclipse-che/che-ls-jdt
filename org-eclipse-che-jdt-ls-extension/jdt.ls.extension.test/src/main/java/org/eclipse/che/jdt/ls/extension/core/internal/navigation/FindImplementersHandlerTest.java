d3852893998036110040ee2964c5762647499d68
Anatoliy Bazko <abazko@redhat.com>
2018-08-07 15:30:11 +0300

    Formatting and applying a new license
    Signed-off-by: Anatoliy Bazko <abazko@redhat.com>

--- org-eclipse-che-jdt-ls-extension/jdt.ls.extension.test/src/main/java/org/eclipse/che/jdt/ls/extension/core/internal/navigation/FindImplementersHandlerTest.java
+++ org-eclipse-che-jdt-ls-extension/jdt.ls.extension.test/src/main/java/org/eclipse/che/jdt/ls/extension/core/internal/navigation/FindImplementersHandlerTest.java
@@ -1,14 +1,14 @@
 /*
  * Copyright (c) 2012-2018 Red Hat, Inc.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
  */
-
 package org.eclipse.che.jdt.ls.extension.core.internal.navigation;
 
 import static java.util.Collections.singletonList;
@@ -42,7 +42,7 @@ public class FindImplementersHandlerTest extends AbstractProjectsManagerBasedTes
   public void testFindInterfaceMethodImplementers() {
     String fileURI = createFileUri("src/main/java/org/eclipse/che/examples/TestInterface.java");
     TextDocumentPositionParams params =
-        new TextDocumentPositionParams(new TextDocumentIdentifier(fileURI), new Position(14, 10));
+        new TextDocumentPositionParams(new TextDocumentIdentifier(fileURI), new Position(15, 10));
     ImplementersResponse impls = getImplementers(singletonList(params), new NullProgressMonitor());
     assertEquals(1, impls.getImplementers().size());
     assertEquals("interfaceMethod()", impls.getImplementers().get(0).getName());
@@ -55,7 +55,7 @@ public class FindImplementersHandlerTest extends AbstractProjectsManagerBasedTes
   public void testFindInterfaceImplementers() {
     String fileURI = createFileUri("src/main/java/org/eclipse/che/examples/TestInterface.java");
     TextDocumentPositionParams params =
-        new TextDocumentPositionParams(new TextDocumentIdentifier(fileURI), new Position(12, 22));
+        new TextDocumentPositionParams(new TextDocumentIdentifier(fileURI), new Position(13, 22));
     ImplementersResponse impls = getImplementers(singletonList(params), new NullProgressMonitor());
     assertEquals(2, impls.getImplementers().size());
     assertEquals("ParentClass", impls.getImplementers().get(0).getName());
@@ -66,7 +66,7 @@ public class FindImplementersHandlerTest extends AbstractProjectsManagerBasedTes
   public void testFindClassMethodImplementers() {
     String fileURI = createFileUri("src/main/java/org/eclipse/che/examples/ParentClass.java");
     TextDocumentPositionParams params =
-        new TextDocumentPositionParams(new TextDocumentIdentifier(fileURI), new Position(14, 24));
+        new TextDocumentPositionParams(new TextDocumentIdentifier(fileURI), new Position(15, 24));
     ImplementersResponse impls = getImplementers(singletonList(params), new NullProgressMonitor());
     assertEquals(1, impls.getImplementers().size());
     assertEquals("simpleMethod(int)", impls.getImplementers().get(0).getName());
@@ -76,7 +76,7 @@ public class FindImplementersHandlerTest extends AbstractProjectsManagerBasedTes
   public void testFindClassImplementers() {
     String fileURI = createFileUri("src/main/java/org/eclipse/che/examples/ParentClass.java");
     TextDocumentPositionParams params =
-        new TextDocumentPositionParams(new TextDocumentIdentifier(fileURI), new Position(12, 29));
+        new TextDocumentPositionParams(new TextDocumentIdentifier(fileURI), new Position(13, 29));
     ImplementersResponse impls = getImplementers(singletonList(params), new NullProgressMonitor());
     assertEquals(1, impls.getImplementers().size());
     assertEquals("ChildClass", impls.getImplementers().get(0).getName());
@@ -96,7 +96,7 @@ public class FindImplementersHandlerTest extends AbstractProjectsManagerBasedTes
   public void testNoImplementersExist() {
     String fileURI = createFileUri("src/main/java/org/eclipse/che/examples/ChildClass.java");
     TextDocumentPositionParams params =
-        new TextDocumentPositionParams(new TextDocumentIdentifier(fileURI), new Position(12, 20));
+        new TextDocumentPositionParams(new TextDocumentIdentifier(fileURI), new Position(13, 20));
     ImplementersResponse impls = getImplementers(singletonList(params), new NullProgressMonitor());
     assertEquals(0, impls.getImplementers().size());
   }
--- org-eclipse-che-jdt-ls-extension/jdt.ls.extension.test/src/main/java/org/eclipse/che/jdt/ls/extension/core/internal/testdetection/TestDetectionHandlerTest.java
+++ org-eclipse-che-jdt-ls-extension/jdt.ls.extension.test/src/main/java/org/eclipse/che/jdt/ls/extension/core/internal/testdetection/TestDetectionHandlerTest.java
@@ -1,9 +1,10 @@
 /*
  * Copyright (c) 2012-2018 Red Hat, Inc.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
@@ -94,7 +95,7 @@ public class TestDetectionHandlerTest extends AbstractProjectsManagerBasedTest {
     TestPosition testPosition = result.get(0);
 
     assertEquals("shouldSuccessOfAppOne", testPosition.getTestName());
-    assertEquals(697, testPosition.getTestNameStartOffset());
+    assertEquals(673, testPosition.getTestNameStartOffset());
     assertEquals(21, testPosition.getTestNameLength());
     assertEquals(104, testPosition.getTestBodyLength());
   }
